56233149650d410c8257de93c7a723815a11e469
hirohitoqwe <balduin@internet.ru>
2023-11-18 01:31:35 +0300

    init

---
+++ resources/js/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            name: "Isaac"
+        }
+    }
+}
+</script>
+
+<template>
+    <h3>Hi! {{ this.name }}</h3>
+</template>
+
+<style scoped>
+
+</style>
